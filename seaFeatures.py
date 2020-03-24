5503c97c7381d6ecfda18a8346bdae1fbba37336
sourav Bhandari <sourav.b12b@gmail.com>
2020-03-24 13:33:04 +0530

    done

--- seaFeatures.py
+++ seaFeatures.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 sns.set_style("whitegrid")
-df=pd.read_csv("market_fact.csv")
+df=pd.read_csv("./global_sales_data/market_fact.csv")
 '''
 sns.distplot(df['Shipping_Cost'])
 plt.show()
 '''
 #subplots 
-
+'''
 plt.subplot(2,2,1)
 plt.title('sales')
 sns.distplot(df['Sales'])
@@ -29,4 +29,28 @@ plt.subplot(2,2,4)
 #plt.title('profit')
 sns.distplot(df['Shipping_Cost'])
 plt.show()
+'''
 # box plot
+'''sns.boxplot(df['Order_Quantity']) # it is default on x-axis
+plt.show()
+# for y axis
+sns.boxplot(y=df['Order_Quantity'])
+plt.show()
+'''
+
+#sns.jointplot('Sales','Profit',df)
+#plt.show() #otuput is heavyily skewed so we aplly condn
+'''
+df=df[(df.Profit<10000) &(df.Sales<20000)]
+sns.jointplot('Sales','Profit',df,kind="hex",color="k")
+plt.show() 
+'''
+
+   ''' btc = pd.read_csv("Crypto_data/bitcoin_price.csv")
+    ether = pd.read_csv("Crypto_data/ethereum_price.csv")
+    monero = pd.read_csv("Crypto_data/monero_price.csv")
+    neo = pd.read_csv("Crypto_data/neo_price.csv")
+    quantum=pd.read_csv("Crypto_data/qtum_price.csv")
+    ripple=pd.read_csv("Crypto_data/ripple_price.csv")
+
+read from telcov.ipynb file'''
